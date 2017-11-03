38e9be343a9af8c34199d702c812146f39aa77b4
Samuel Biondolillo <biwinning121@gmail.com>
2017-11-03 15:48:49 -0400

    set up basic framework for choosing files

--- src/main/TestFilePicker.java
+++ src/main/TestFilePicker.java
@@ -1,9 +1,39 @@
 package main;
 
+import java.io.File;
+import javax.swing.JFileChooser;
+
 public class TestFilePicker {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		JFileChooser chooser = new JFileChooser("./bin");
+		
+		int result = chooser.showOpenDialog(null);
+		
+        if (result == JFileChooser.APPROVE_OPTION){
+        	
+            File openFile = chooser.getSelectedFile();
+            System.out.println(openFile);
+            
+        } else if (result == JFileChooser.CANCEL_OPTION) {
+        	
+        	System.out.println("Sorry, you didn't select a file to open.");
+        	
+        }
+        
+        int saveResult = chooser.showSaveDialog(null);
+        
+        if (saveResult == JFileChooser.APPROVE_OPTION){
+        	
+            File saveFile = chooser.getSelectedFile();
+            System.out.println(saveFile);
+            
+        } else if (saveResult == JFileChooser.CANCEL_OPTION) {
+        	
+        	System.out.println("Sorry, you didn't select a file to save.");
+        	
+        }
 
 	}
 
