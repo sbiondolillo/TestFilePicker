ced131eb887c4c398fa234120b4e4383634afc54
Samuel Biondolillo <biwinning121@gmail.com>
2017-11-03 16:03:10 -0400

    add method to create files when saving

--- src/main/TestFilePicker.java
+++ src/main/TestFilePicker.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.io.File;
+import java.io.IOException;
+
 import javax.swing.JFileChooser;
 
 public class TestFilePicker {
@@ -14,7 +16,16 @@ public class TestFilePicker {
         if (result == JFileChooser.APPROVE_OPTION){
         	
             File openFile = chooser.getSelectedFile();
-            System.out.println(openFile);
+            System.out.println("You selected: " + openFile.getPath());
+            if (openFile.canRead()) {
+            	
+            	System.out.println("Great, you can read from this file!");
+            }
+            else {
+            	
+            	System.out.println("Sorry, you can't read from this file!");
+            	
+            }
             
         } else if (result == JFileChooser.CANCEL_OPTION) {
         	
@@ -27,7 +38,33 @@ public class TestFilePicker {
         if (saveResult == JFileChooser.APPROVE_OPTION){
         	
             File saveFile = chooser.getSelectedFile();
-            System.out.println(saveFile);
+            System.out.println("You selected: " + saveFile.getPath());
+            if (saveFile.canWrite()) {
+            	
+            	System.out.println("Great, you can write to this file!");
+            }
+            else {
+            	
+            	if (saveFile.exists()) {
+            	
+            		System.out.println("Sorry, you can't write to this file!");
+            		
+            	} else {
+            		
+            		try {
+            			
+            			saveFile.createNewFile();
+            			System.out.println("Great, you can write to this file!");
+            			
+            		}
+            		catch (IOException e) {
+            			
+            			System.out.println("Sorry, you won't be able to save to this file.");
+            		}
+            		
+            	}
+            	
+            }
             
         } else if (saveResult == JFileChooser.CANCEL_OPTION) {
         	
